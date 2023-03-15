40ca0529e35164dee42a803fc8a9fd004705c296
Ryan Murphy <ryanmurphy@RPM-MBP.local>
2023-03-15 16:19:11 -0500

    finished Angular exercises 1

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinks = ['https://www.apple.com/', 'https://www.facebook.com/'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'My Images';
+  photosTitle = 'Random Images';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
   image2 = 'https://4.bp.blogspot.com/-lH8lmsyN9kE/UAkL17rkI1I/AAAAAAAAAbo/Nwc4phMuxuE/s1600/pipe+organ.jpg';
   image3 = 'https://www.klaviano.com/files/images/double%20keyboard.jpg';
